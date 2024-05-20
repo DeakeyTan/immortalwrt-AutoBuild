e7caf68a65899081c39a664e615901ad08365dff
DeakeyTan <40329013+DeakeyTan@users.noreply.github.com>
2024-05-20 11:50:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,7 +22,7 @@
 #预置OpenClash内核和数据
 if [ -d *"OpenClash"* ]; then
 	CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
-	CORE_TYPE="amd64"
+	CORE_TYPE="amd64-v3"
 	CORE_TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 
 	CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-$CORE_TYPE.tar.gz"
