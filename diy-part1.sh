fcfc0427589faef05b3c9f6d9556040d5279f194
DeakeyTan <40329013+DeakeyTan@users.noreply.github.com>
2025-05-16 14:39:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,4 @@
 # Add a feed source
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-# echo 'src-git adguardhomeofficial https://github.com/AdguardTeam/AdGuardHome.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
